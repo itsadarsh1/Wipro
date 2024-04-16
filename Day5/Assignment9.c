f807ed0866a7d7048505f3bb505e6c150676a13a
Adarsh kumar <60160375+itsadarsh1@users.noreply.github.com>
2024-04-16 21:50:38 +0530

    Create Assignment9.c

---
+++ Day5/Assignment9.c
@@ -0,0 +1,41 @@
+/Input: 5
+Output:
+         A
+        ABA
+       ABCBA
+      ABCDCBA
+    ABCDEDCBA/
+
+#include <stdio.h>
+
+int main() 
+{
+    int n,i,j;
+    char ch;
+
+    printf("Enter n:");
+    scanf("%d",&n);
+
+    for (i=0;i<n;i++) 
+    {
+        for (j=0;j<n-i-1;j++) 
+        {
+            printf(" ");
+        }
+        for (ch='A';ch<'A'+i+1;ch++) 
+        {
+            printf("%c", ch);
+            if (ch!='A'+i) 
+            {
+                printf(" ");
+            }
+        }
+        for (ch='A'+i-1;ch>='A';ch--) 
+        {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
