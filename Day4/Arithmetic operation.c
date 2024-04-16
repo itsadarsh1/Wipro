a5929a9a4d40477fd614d66154a7052bd3bcdbb1
Adarsh kumar <60160375+itsadarsh1@users.noreply.github.com>
2024-04-16 22:58:11 +0530

    Add files via upload

---
+++ Day4/Arithmetic operation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{ 
+    int n1,n2,r1,r2,r3; float r4;
+printf("Enter the first number:");
+scanf("%d", &n1);
+printf("Enter the second number:");
+scanf("%d", &n2);
+printf("Addtion of %d and %d is:%d",n1,n2,r1=n1+n2);
+printf("\nsubstraction of %d and %d is:%d",n1,n2,r2=n1-n2);
+printf("\nmultiplication of %d and %d is:%d",n1,n2,r3=n1*n2);
+printf("\ndivision of %d and %d is:%f",n1,n2,r4=n1/n2);
+
+ return 0;
+ }
